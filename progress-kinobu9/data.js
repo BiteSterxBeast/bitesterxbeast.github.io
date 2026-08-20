29788d2c0fe7674bed99a427c67c5d8b6faaee97
BiteSterxBeast <joshuamelendez2008@gmail.com>
2026-08-20 01:17:33 -0400

    Change goal amount from 1000 to 20

--- progress-kinobu9/data.js
+++ progress-kinobu9/data.js
@@ -4,4 +4,4 @@
 // ============================================
 
 var CURRENT_AMOUNT = 6;   // how much has been made so far
-var GOAL_AMOUNT = 1000;     // the target goal
+var GOAL_AMOUNT = 20;     // the target goal
